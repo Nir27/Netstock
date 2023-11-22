2d010b414cb48842ad314d8291b490a779c44da1
Nirodha Buddhima <nirodhabuddhima@gmail.com>
2023-11-22 17:23:59 +0530

    commits

--- inventory-management/src/app/search-inventory/search-inventory.component.ts
+++ inventory-management/src/app/search-inventory/search-inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgModel} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-search-inventory',
@@ -8,11 +9,25 @@ import {NgModel} from "@angular/forms";
 })
 export class SearchInventoryComponent {
 
+  constructor(private http:HttpClient) {
+  }
+
+
   type:string="";
   brands:string[]=[];
   description: string="";
   onSearchInventory(){
 
+    console.log(this.type);
+    console.log(this.brands);
+    console.log(this.description);
 
+
+
+    this.http.post("http://localhost:8080/inventory?",{ responseType: 'text' }).subscribe((resultData:any)=>{
+
+        console.log(resultData);
+
+    });
   }
 }
